a136ffa7b13cc417d1a42472462fd955fb90321e
Mike Maat <mmaat@ualberta.ca>
2016-04-11 09:46:36 -0600

    Update dsc-zone-contact.groovy

--- devicetypes/dsc/dsc-zone-contact.src/dsc-zone-contact.groovy
+++ devicetypes/dsc/dsc-zone-contact.src/dsc-zone-contact.groovy
@@ -4,11 +4,12 @@
  *  Author: Jordan <jordan@xeron.cc>
  *  Original Author: Matt Martz <matt.martz@gmail.com>
  *  Date: 2016-02-27
+ *  Cosmetically Tweaked By: Mike Maat <mmaat@ualberta.ca> on 2016-04-08
  */
 
 // for the UI
 metadata {
-  definition (name: "DSC Zone Contact", author: "jordan@xeron.cc", namespace: 'dsc') {
+  definition (name: "DSC Zone Contact", author: "jordan@xeron.cc", namespace: 'DSC') {
     // Change or define capabilities here as needed
     capability "Contact Sensor"
     capability "Sensor"
@@ -24,25 +25,29 @@ metadata {
   }
 
   tiles(scale: 2) {
-    standardTile ("zone", "device.contact", width: 4, height: 4, title: "Zone") {
-      state "open",   label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ffa81e"
-      state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
-      state "alarm",  label: '${name}', icon: "st.contact.contact.open",   backgroundColor: "#ff0000"
-     }
-     standardTile ("trouble", "device.trouble", width: 2, height: 2, title: "Trouble") {
-       state "restore", label: 'No\u00A0Trouble', icon: "st.security.alarm.clear", backgroundColor: "#79b821"
+  	multiAttributeTile(name:"zone", type: "generic", width: 6, height: 4){
+        tileAttribute ("device.contact", key: "PRIMARY_CONTROL") {
+            attributeState "open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ffa81e"
+            attributeState "closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#79b821"
+            attributeState "alarm", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#ff0000"
+        }
+    }
+	standardTile ("trouble", "device.trouble", width: 2, height: 2, title: "Trouble") {
+       state "restore", label: 'No Trouble', icon: "st.security.alarm.clear"
        state "tamper", label: 'Tamper', icon: "st.security.alarm.alarm", backgroundColor: "#ffa81e"
        state "fault", label: 'Fault', icon: "st.security.alarm.alarm", backgroundColor: "#ff1e1e"
-     }
-     standardTile("bypass", "capability.momentary", width: 2, height: 2, title: "Bypass"){
-       state "bypass", label: 'Bypass', action: "bypass", icon: "st.locks.lock.unlocked", backgroundColor: "#FFFF00"
-     }
-
+	}
+	standardTile("bypass", "capability.momentary", width: 2, height: 2, title: "Bypass", decoration: "flat"){
+       state "bypass", label: 'Bypass', action: "bypass", icon: "st.locks.lock.unlocked"
+	}
+    standardTile("spacerTile", "spacerTile", decoration: "flat", width: 1, height: 2) {
+	}
+    
     // This tile will be the tile that is displayed on the Hub page.
     main "zone"
 
     // These tiles will be displayed when clicked on the device, in the order listed here.
-    details(["zone", "trouble", "bypass"])
+    details(["zone", "spacerTile", "trouble", "bypass", "spacerTile"])
   }
 }
 
