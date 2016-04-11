8297003dffb432aa946a34b2b47573ef6f053ec3
Mike Maat <mmaat@ualberta.ca>
2016-04-11 10:40:19 -0600

    Update dsc-zone-smoke-2w.groovy

--- devicetypes/dsc/dsc-zone-smoke-2w.src/dsc-zone-smoke-2w.groovy
+++ devicetypes/dsc/dsc-zone-smoke-2w.src/dsc-zone-smoke-2w.groovy
@@ -10,41 +10,41 @@
 
 // for the UI
 metadata {
-  definition (name: "DSC Zone Smoke 2w", author: "jordan@xeron.cc", namespace: 'DSC') {
-    // Change or define capabilities here as needed
-    capability "Smoke Detector"
-    capability "Sensor"
-    capability "Momentary"
+	definition (name: "DSC Zone Smoke 2w", author: "jordan@xeron.cc", namespace: 'DSC') {
+		// Change or define capabilities here as needed
+		capability "Smoke Detector"
+		capability "Sensor"
+		capability "Momentary"
 
-    // Add commands as needed
-    command "zone"
-    command "bypass"
-  }
+		// Add commands as needed
+		command "zone"
+		command "bypass"
+	}
 
-  tiles {
-    // Main Row
-    multiAttributeTile(name:"zone", type: "generic", width: 6, height: 4){
-      tileAttribute ("device.smoke", key: "PRIMARY_CONTROL") {
-        attributeState "clear", label:'Clear', icon:"st.alarm.smoke.clear", backgroundColor:"#ffffff"
-        attributeState "smoke", label:'Smoke', icon:"st.alarm.smoke.smoke", backgroundColor:"#e86d13"
-        attributeState "tested", label:'Tested', icon:"st.alarm.smoke.test", backgroundColor:"#e86d13"
-      }
-    }
+	tiles {
+		// Main Row
+		multiAttributeTile(name:"zone", type: "generic", width: 6, height: 4){
+			tileAttribute ("device.smoke", key: "PRIMARY_CONTROL") {
+				attributeState "clear", label:'Clear', icon:"st.alarm.smoke.clear", backgroundColor:"#ffffff"
+				attributeState "smoke", label:'Smoke', icon:"st.alarm.smoke.smoke", backgroundColor:"#e86d13"
+				attributeState "tested", label:'Tested', icon:"st.alarm.smoke.test", backgroundColor:"#e86d13"
+			}
+		}
 
-    // This tile will be the tile that is displayed on the Hub page.
-    main "zone"
+		// This tile will be the tile that is displayed on the Hub page.
+		main "zone"
 
-    // These tiles will be displayed when clicked on the device, in the order listed here.
-    details "zone"
-  }
+		// These tiles will be displayed when clicked on the device, in the order listed here.
+		details "zone"
+	}
 }
 
 // handle commands
 def zone(String state) {
-  def text = null
-  def results = []
-  // state will be a valid state for a zone (open, closed)
-  // zone will be a number for the zone
-  log.debug "Zone: ${state}"
-  sendEvent (name: "smoke", value: "${state}")
+	def text = null
+	def results = []
+	// state will be a valid state for a zone (open, closed)
+	// zone will be a number for the zone
+	log.debug "Zone: ${state}"
+	sendEvent (name: "smoke", value: "${state}")
 }
